dfbd1c499448645629f53fb32f17b319fc72aa28
Owen Mundy <omundy@gmail.com>
2022-02-21 15:16:17 -0500

    Rewrite 2d platformer scripts using different sources

---
+++ Assets/Sprite_Anim_Fred/Scripts/PlayerController.cs
@@ -0,0 +1,173 @@
+using System;
+using UnityEngine;
+
+/**
+ *  Basic 2D platformer controller using
+ *  https://www.youtube.com/playlist?list=PLPV2KyIb3jR6TFcFuzI2bB7TMNIIBpKMQ
+ *  https://craftgames.co/unity-2d-platformer-movement/
+ */
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Input")]
+
+    // horizontal movement speed and direction
+    public Vector2 playerInput;
+    // boolean to detect / test jump input
+    [SerializeField] bool jumpPress = false;
+    [SerializeField] bool jumpHold = false;
+
+
+    [Header("Parameters")]
+
+    [SerializeField] private LayerMask groundLayer; // A mask determining what is ground to the character
+    [SerializeField] private Transform ceilingCheck; // A position marking where to check for ceilings
+    [SerializeField] private Transform groundCheck; // A position marking where to check if the player is isGrounded.
+
+    private Vector3 velocity = Vector3.zero;
+    public float runSpeed = 30f;
+    [SerializeField] private float jumpForce = 6.2f; // Force added when the player jumps
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 3f;
+
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f; // How much to smooth out the movement
+    [SerializeField] private bool airControl = true; // Whether or not a player can steer while jumping
+
+    const float isGroundedRadius = .2f; // Radius of the overlap circle to determine if isGrounded
+
+    public float rememberGroundedFor = 0.1f;
+    float lastTimeGrounded;
+
+    private Rigidbody2D rb2d;
+
+    [Header("Display")]
+
+    public Animator animator;
+    [SerializeField] bool facingRight = true;  // For determining which way the player is currently facing.
+    public bool isGrounded; // is the player on the ground?
+
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        if (!groundCheck || !ceilingCheck)
+        {
+            Debug.LogError("GroundCheck and CeilingCheck required");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+
+    private void Update()
+    {
+        // get horizontal input (between -1 and 1) from player
+        playerInput.x = Input.GetAxisRaw("Horizontal");
+        Move(playerInput.x * runSpeed * Time.fixedDeltaTime);
+
+        // check if jump keys / buttons are pressed on this loop
+        jumpPress = Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.UpArrow);
+        Jump();
+        // check if jump keys / buttons are held down
+        jumpHold = Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow);
+        BetterJump();
+
+        // whether currently on ground (collision detection)
+        CheckIfGrounded();
+    }
+
+    public void Move(float move)
+    {
+        // set the speed variable
+        animator.SetFloat("Speed", Mathf.Abs(move));
+
+        //only control the player if isGrounded or airControl is turned on
+        if (isGrounded || airControl)
+        {
+            // Move the character by finding the target velocity
+            Vector3 targetVelocity = new Vector2(move * 10f, rb2d.velocity.y);
+            // And then smoothing it out and applying it to the character
+            rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+            // If the input is moving the player right and the player is facing left...
+            if ((move > 0 && !facingRight) || (move < 0 && facingRight))
+            {
+                Flip();
+            }
+        }
+    }
+
+    public void Jump()
+    {
+        // if jump button pressed + either grounded or late jump off edge 
+        if (jumpPress && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor))
+        {
+            // Add a vertical force to the player 
+            // => Brackeys version: e.g. jumpForce = 550, rb2d.gravityScale = 3
+            //rb2d.AddForce(new Vector2(0f, jumpForce));
+
+            // Add a vertical force to the player 
+            // => craftgames version: e.g. jumpForce = 6, rb2d.gravityScale = 1
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+
+            // reset for late jump
+            lastTimeGrounded = Time.time;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+    // adjust falling and jumping speed based on whether player still has jump pressed
+    // for more see: https://www.youtube.com/watch?v=7KiK0Aqtmzc
+    void BetterJump()
+    {
+        // slow down velocity for all
+        if (rb2d.velocity.y < 0)
+        {
+            rb2d.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        // slow up velocity if they release early
+        else if (rb2d.velocity.y > 0 && !jumpHold)
+        {
+            rb2d.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void CheckIfGrounded()
+    {
+        // is currently grounded?
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+
+        // Check if a circle cast to the groundcheck position hits anything designated as Ground
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, isGroundedRadius, groundLayer);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                // if not parent collider then set true 
+                isGrounded = true;
+                // if wasn't already on ground then it just landed
+                if (!wasGrounded)
+                {
+                    //Debug.Log("collided with: " + colliders[i].gameObject.name);
+                    OnLanding();
+                }
+            }
+        }
+    }
+
+    void OnLanding()
+    {
+        //Debug.Log("OnLanding");
+        animator.SetBool("IsJumping", false);
+    }
+}
--- README.md
+++ README.md
@@ -18,5 +18,13 @@ A sample Unity (2021.2.7f1) project that shows
 
 Basic sprite animation and character controller
 
-- Uses [pixeldudesmaker generator](https://0x72.itch.io/pixeldudesmaker) by [0x72](https://0x72.itch.io/)
-- See [Assets/Sprite_Anim_Fred/README.md](Assets/Sprite_Anim_Fred/README.md) for more info
+Artwork credit: [pixeldudesmaker generator](https://0x72.itch.io/pixeldudesmaker) by [0x72](https://0x72.itch.io/)
+
+Scripts created using elements of the following:
+- Brackeys [How to make a 2D Game series](https://www.youtube.com/playlist?list=PLPV2KyIb3jR6TFcFuzI2bB7TMNIIBpKMQ):
+    - [2D Movement in Unity](https://www.youtube.com/watch?v=dwcT-Dch0bA&list=PLPV2KyIb3jR6TFcFuzI2bB7TMNIIBpKMQ&index=3) (21:08), [github.com/Brackeys/2D-Movement](https://github.com/Brackeys/2D-Movement)
+    - [2D Animation in Unity](https://www.youtube.com/watch?v=hkaysu1Z-N8&list=PLPV2KyIb3jR6TFcFuzI2bB7TMNIIBpKMQ&index=4) (21:52) [github.com/Brackeys/2D-Animation](https://github.com/Brackeys/2D-Animation)
+    - [2D Camera in Unity](https://www.youtube.com/watch?v=2jTY11Am0Ig&list=PLPV2KyIb3jR6TFcFuzI2bB7TMNIIBpKMQ&index=5) (9:18) [github.com/Brackeys/2D-Camera](https://github.com/Brackeys/2D-Camera)
+- Craft Games [Unity 2D Platformer Movement (Beginner Friendly Tutorial)](https://craftgames.co/unity-2d-platformer-movement/)
+- Sharp Coder [Unity 2D Character Controller](https://sharpcoderblog.com/blog/2d-platformer-character-controller)
+- Benjamin Dendas [How to create a simple Character Controller for a 2D Unity project](https://medium.com/@benjamindendas/how-to-create-a-simple-character-controller-for-a-2d-unity-project-334221b6ecf3) and [code](https://github.com/BenjaminDendas/mini2d)
